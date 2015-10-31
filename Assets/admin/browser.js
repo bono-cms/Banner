3f74ba59780e8d27035d91a6d63e5f9fb77411f0
David Yang <daworld.ny@gmail.com>
2015-10-31 06:51:33 +0500

    Removed boilerplate

--- Assets/admin/browser.js
+++ Assets/admin/browser.js
@@ -1,6 +1,4 @@
-
 $(function(){
-	
 	$.delete({
 		categories : {
 			banner : {
@@ -8,21 +6,4 @@ $(function(){
 			}
 		}
 	});
-	
-	
-	$("[data-button='remove-selected']").click(function() {
-		$.ajax({
-			url : "/admin/module/banner/delete-selected.ajax",
-			data : $("form").serialize(),
-			success : function(response){
-				if (response == "1") {
-					window.location.reload();
-				} else {
-					console.log(response);
-				}
-			}
-		});
-	});
-	
-	
 });
