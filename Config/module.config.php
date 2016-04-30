9933660a7c389d8af68cbd1275764d8413320a0a
David Yang <daworld.ny@gmail.com>
2016-04-30 22:35:40 +0500

    Replaced raw path with route

--- Config/module.config.php
+++ Config/module.config.php
@@ -12,7 +12,7 @@
 return array(
     'module' => array(
         'name'  => 'Banner',
-        'url'   => '/admin/module/banner',
+        'route' => 'Banner:Admin:Banner@gridAction',
         'icon'  => 'fa fa-file-image-o fa-5x',
         'order' => 1,
         'description' => 'Banner module allows you to manage random banners of different formats on your site'
