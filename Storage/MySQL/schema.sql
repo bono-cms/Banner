1bb0f395a8758d00a69378628211b9dd61ac67be
David Yang <daworld.ny@gmail.com>
2017-07-30 22:03:30 +0500

    Schema for categories

--- Storage/MySQL/schema.sql
+++ Storage/MySQL/schema.sql
@@ -2,10 +2,20 @@
 DROP TABLE IF EXISTS `bono_module_banner`;
 CREATE TABLE `bono_module_banner` (
 	
-	`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `category_id` INT NOT NULL,
 	`lang_id` INT NOT NULL,
 	`name` varchar(254) NOT NULL,
 	`link` varchar(254) NOT NULL,
 	`file` varchar(254) NOT NULL
 	
+) DEFAULT CHARSET = UTF8;
+
+
+DROP TABLE IF EXISTS `bono_module_banner_categories`;
+CREATE TABLE `bono_module_banner_categories` (
+
+	`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	`name` varchar(255) NOT NULL
+
 ) DEFAULT CHARSET = UTF8;
