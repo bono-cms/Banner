d94be7c4f31ec8fcfb7724122ec7ff78223beb80
dave-yang <daworld.ny@gmail.com>
2018-12-17 18:52:26 +0500

    Minor fixes. Use shared action partial

--- Config/module.config.php
+++ Config/module.config.php
@@ -4,7 +4,6 @@ return array(
  'caption'  => 'Banner',
  'name'  => 'Banner',
  'route' => 'Banner:Admin:Banner@gridAction',
- 'icon'  => 'fa fa-file-image-o fa-5x',
  'order' => 1,
  'description' => 'Banner module allows you to manage random banners of different formats on your site'
 );
