3f08dbe88b64181ff4e18df2ff4b2afd51ac8e91
dave-yang <daworld.ny@gmail.com>
2019-03-05 23:13:57 +0500

    Updated module config

--- Config/module.config.php
+++ Config/module.config.php
@@ -1,9 +1,28 @@
 <?php
 
+/**
+ * Module configuration container
+ */
+
 return array(
- 'caption'  => 'Banner',
- 'name'  => 'Banner',
- 'route' => 'Banner:Admin:Banner@gridAction',
- 'order' => 1,
- 'description' => 'Banner module allows you to manage random banners of different formats on your site'
+    'caption'  => 'Banner',
+    'description' => 'Banner module allows you to manage random banners of different formats on your site',
+    'menu' => array(
+        'name'  => 'Banner',
+        'icon' => 'fab fa-adversal',
+        'items' => array(
+            array(
+                'route' => 'Banner:Admin:Banner@gridAction',
+                'name' => 'View all banners'
+            ),
+            array(
+                'route' => 'Banner:Admin:Banner@addAction',
+                'name' => 'Add new banner'
+            ),
+            array(
+                'route' => 'Banner:Admin:Category@addAction',
+                'name' => 'Add new category'
+            )
+        )
+    )
 );
